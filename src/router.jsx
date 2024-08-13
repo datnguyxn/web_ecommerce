44958aa2d95f21fd7bdd774aee2a1b3a878265ab
datnguyxn <nguyentrongdat1108@gmail.com>
2024-08-13 20:19:14 +0700

    feat: login register ui

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register  from "./views/register.jsx";
 import GuestLayout from "./components/GuestLayout.jsx";
+import Login from "./views/login.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <GuestLayout />,
         children: [
+            {
+                path: "/login",
+                element: <Login />
+            },
             {
                 path: "/register",
                 element: <Register />
--- src/views/login.jsx
+++ src/views/login.jsx
@@ -3,22 +3,20 @@ import { useRef } from "react";
 import { useStateContext } from "../contexts/contextProvider";
 import axiosClient from "../axiosClient";
 
-export default function Register() {
-    const nameRef = useRef();
-    const passwordRef = useRef();
+export default function login() {
     const emailRef = useRef();
+    const passwordRef = useRef();
 
     const { setUser, setToken } = useStateContext();
 
     const Submit = (e) => {
         e.preventDefault();
         const payload = {
-            name: nameRef.current.value,
             email: emailRef.current.value,
             password: passwordRef.current.value
         }
 
-        axiosClient.post('/register', payload)
+        axiosClient.post('/login', payload)
                     .then((data) => { 
                         setUser(data.user);
                         setToken(data.token);
@@ -30,17 +28,15 @@ export default function Register() {
                         }
                     });
     }
-
     return (
         <div className="login-signup-form animated fadeinDown">
             <div className="form">
-                <h1 className="title">Create A New Account</h1>
+                <h1 className="title">Login</h1>
                 <form onSubmit={Submit}>
-                    <input type="name" ref={nameRef} placeholder="Name" required/>
-                    <input type="email" ref={emailRef} placeholder="Email" required/>
-                    <input type="password" ref={passwordRef} placeholder="Password" required/>
-                    <button type="submit" className="btn btn-block">Register</button>
-                    <p className="message">Already Have An Account? <Link to='/login'>Login</Link></p>
+                    <input type="email" placeholder="Email" required/>
+                    <input type="password" placeholder="Password" required/>
+                    <button type="submit" className="btn btn-block">Login</button>
+                    <p className="message">Don't Have An Account? <Link to='/register'>Register</Link></p>
                 </form>
             </div>
         </div>
